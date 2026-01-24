390c8df189b2cadf960c0ada757ebb00154c7082
Piotr Rzeszut <piotrrze@gmail.com>
2026-01-24 21:42:36 +0100

    Fix for esphome 2026.1.1 reported in #18

--- components/sinclair_ac/esppac.cpp
+++ components/sinclair_ac/esppac.cpp
@@ -250,7 +250,11 @@ void SinclairAC::set_current_temperature_sensor(sensor::Sensor *current_temperat
 void SinclairAC::set_vertical_swing_select(select::Select *vertical_swing_select)
 {
     this->vertical_swing_select_ = vertical_swing_select;
-    this->vertical_swing_select_->add_on_state_callback([this](const std::string &value, size_t index) {
+    this->vertical_swing_select_->add_on_state_callback([this](size_t index) {
+        auto selected = this->vertical_swing_select_->at(index);
+        if (!selected.has_value())
+            return;
+        auto &value = selected.value();
         if (value == this->vertical_swing_state_)
             return;
         this->on_vertical_swing_change(value);
@@ -260,7 +264,11 @@ void SinclairAC::set_vertical_swing_select(select::Select *vertical_swing_select
 void SinclairAC::set_horizontal_swing_select(select::Select *horizontal_swing_select)
 {
     this->horizontal_swing_select_ = horizontal_swing_select;
-    this->horizontal_swing_select_->add_on_state_callback([this](const std::string &value, size_t index) {
+    this->horizontal_swing_select_->add_on_state_callback([this](size_t index) {
+        auto selected = this->horizontal_swing_select_->at(index);
+        if (!selected.has_value())
+            return;
+        auto &value = selected.value();
         if (value == this->horizontal_swing_state_)
             return;
         this->on_horizontal_swing_change(value);
@@ -270,7 +278,11 @@ void SinclairAC::set_horizontal_swing_select(select::Select *horizontal_swing_se
 void SinclairAC::set_display_select(select::Select *display_select)
 {
     this->display_select_ = display_select;
-    this->display_select_->add_on_state_callback([this](const std::string &value, size_t index) {
+    this->display_select_->add_on_state_callback([this](size_t index) {
+        auto selected = this->display_select_->at(index);
+        if (!selected.has_value())
+            return;
+        auto &value = selected.value();
         if (value == this->display_state_)
             return;
         this->on_display_change(value);
@@ -280,7 +292,11 @@ void SinclairAC::set_display_select(select::Select *display_select)
 void SinclairAC::set_display_unit_select(select::Select *display_unit_select)
 {
     this->display_unit_select_ = display_unit_select;
-    this->display_unit_select_->add_on_state_callback([this](const std::string &value, size_t index) {
+    this->display_unit_select_->add_on_state_callback([this](size_t index) {
+        auto selected = this->display_unit_select_->at(index);
+        if (!selected.has_value())
+            return;
+        auto &value = selected.value();
         if (value == this->display_unit_state_)
             return;
         this->on_display_unit_change(value);
